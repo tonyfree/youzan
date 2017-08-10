c58e8b92a4afd394ecffdf906eb23bb8ab377ac3
tony <fangtao_2008@163.com>
2017-08-10 18:40:51 +0800

    just checout demo

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -27,7 +27,7 @@ new Vue({
       console.log(1111)
       // 加载更多数据
       // this.allLoaded = true;// 若数据已全部获取完毕
-      // this.$refs.loadmore.onBottomLoaded();
+      this.$refs.loadmore.onBottomLoaded()
     },
     handleBottomChange(status) {
       console.log(status)
