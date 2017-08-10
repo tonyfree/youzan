37106d00739f03eeb291ac98662b4f085b7399b3
tony <fangtao_2008@163.com>
2017-08-10 18:31:40 +0800

    mint-ui loadmore

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,10 +5,14 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+import { Loadmore } from 'mint-ui'
+Vue.component(Loadmore.name, Loadmore)
+
 new Vue({
   el: '.container',
   data: {
-    lists: null
+    lists: null,
+    allLoaded: false
   },
   created() {
     this.getLists()
@@ -18,6 +22,15 @@ new Vue({
       axios.post(url.hotLists).then(res => {
         this.lists = res.data.lists
       })
+    },
+    loadBottom() {
+      console.log(1111)
+      // 加载更多数据
+      // this.allLoaded = true;// 若数据已全部获取完毕
+      // this.$refs.loadmore.onBottomLoaded();
+    },
+    handleBottomChange(status) {
+      console.log(status)
     }
   }
 })
