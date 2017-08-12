7fbc5324a49c3aafcd059fba4c208fcea5817379
tony <fangtao_2008@163.com>
2017-08-12 23:19:18 +0800

    create lesson02

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,8 +1,12 @@
 let url = {
   hotLists: '/index/hotLists',
-  banner: '/index/banner'
+  banner: '/index/banner',
+  topList: '/category/topList',
+  subList: '/category/subList',
+  rank: '/category/rank'
 }
 
+//开发环境和真实环境的切换
 // let host = 'http://rapapi.org/mockjsdata/23334'
 let host = 'http://rapapi.org/mockjsdata/24170'
 
