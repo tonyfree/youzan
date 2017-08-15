e18ba5f63d20ddd6e0b8bce283b08701e8a692f1
tony <fangtao_2008@163.com>
2017-08-16 00:57:10 +0800

    add toTop animate

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 import qs from 'qs'
+import Velocity from 'velocity-animate'
 
 import mixin from 'js/mixin.js'
 
@@ -31,6 +32,9 @@ new Vue({
       } else {
         this.show = false
       }
+    },
+    toTop() {
+      Velocity(document.body, "scroll", { duration: 1000 })
     }
   },
   mixins: [mixin]
